4d3e1e69e405cd0fde5c9c1832bb638277ca8e26
Francis Lewis <francis.lewis@me.com>
2019-04-13 11:06:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target')
 }
+
+function increaseRankBy(n) {
+  let ranked = document.querySelectorAll('.ranked-list')
+  for (let r of ranked) {
+    r = parseInt(r) + 1
+  }
+}
