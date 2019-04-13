068d993b44cf68fba2f7edf1b7ade9d6c797e504
Francis Lewis <francis.lewis@me.com>
2019-04-13 11:21:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   let curr = document.querySelector('div#grand-node')
-  console.log(curr[1].firstChild())
+  console.log(curr.firstChild())
   while (true) {
     let next = curr.firstChild()
     if (!next) {
