4a4798ad7c687610bd1fd5d877e923626f9d7265
Francis Lewis <francis.lewis@me.com>
2019-04-13 11:23:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  let ranked = document.querySelectorAll('.ranked-list')
+  for (let r of ranked) {
+    r.innerHTML = parseInt(r.innerHTML) + 1
+  }
+}
+
+function deepestChild() {
+  let curr = document.querySelectorAll('div#grand-node div')
+  return curr[curr.length - 1]
+}
